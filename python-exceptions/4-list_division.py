23d53ce49a2e6a4d7b4a79c1d08745b7e148442b
Oluwaloseyitowi Iji <o.iji@alustudent.com>
2025-03-07 23:22:29 +0200

    p6

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    try:
+        for i in range(list_length):
+            try:
+                quotient = my_list_1[i] / my_list_2[i]
+            except ZeroDivisionError:
+                print("division by 0")
+                quotient = 0
+            except TypeError:
+                print("wrong type")
+                quotient = 0
+            except IndexError:
+                print("out of range")
+                quotient = 0
+            finally:
+                result.append(quotient)
+    finally:
+        return result
+
+# Test cases
+if __name__ == "__main__":
+    my_l_1 = [10, 8, 4]
+    my_l_2 = [2, 4, 4]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
+
+    print("--")
+
+    my_l_1 = [10, 8, 4, 4]
+    my_l_2 = [2, 0, "H", 2, 7]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
---
+++ python-exceptions/README.md
@@ -0,0 +1,1 @@
+Final project and i can sleep
